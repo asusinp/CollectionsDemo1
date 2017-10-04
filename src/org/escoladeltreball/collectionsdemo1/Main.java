de60258319e2d75c213febfcd7bb6e1200c4e8ab
iaw21752927 <alexsusin1998@gmail.com>
2017-10-04 17:15:50 +0200

    LinkedList introduced

--- src/org/escoladeltreball/collectionsdemo1/Main.java
+++ src/org/escoladeltreball/collectionsdemo1/Main.java
@@ -5,6 +5,7 @@ package org.escoladeltreball.collectionsdemo1;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -17,7 +18,7 @@ public class Main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		List<String> list = new ArrayList();
+		List<String> list = new LinkedList<>();
 		
 		list.add("Zulu");
 		list.add("Alpha");
